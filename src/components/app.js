aa6df414eee1db3f0c83313f7d797e7ca32c76bb
Drakethed <boomstick097@gmail.com>
2022-05-27 23:32:28 -0400

    first commit'

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import moment from "moment";
+import {
+  BrowserRouter as Router,
+  Switch, 
+  Route
+} from 'react-router-dom';
+
+
+import NavigationContainer from "./navigation/navigation-container";
+import About from "./pages/about";
+import Home from "./pages/home";
+import Contact from "./pages/Contact";
+import Blog from "./pages/Blog";
+import PortfolioDetail from "./portfolio/portfolio-detail";
+
+
+
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>Chris Is Awesome</h1>
-        <div>{moment().format("MMMM Do YYYY, h:m:ss a")}</div>
+      <div className="app">
+       
+
+        <Router>
+        <div>
+        <h1>Chris Adams' Portfolio</h1>
+        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
+
+        <NavigationContainer />
+        <Switch>
+           <Route exact path="/" component={Home}/>
+           <Route path="/about-me" component={About} />
+           <Route path="/Contact" component={Contact} />
+           <Route path="/Blog" component={Blog} />
+           <Route path="/portfolio/:slug" component={PortfolioDetail} />
+        </Switch>
+        </div>
+        </Router>
+
+
+     
         
       </div>
     );
---
+++ src/components/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PortfolioContainer from '../portfolio/Portfolio-Container'
+
+
+export default function home() {
+  return (
+    <div>
+    <h2>Homepage</h2>
+    <PortfolioContainer />
+    </div>
+  );
+}
